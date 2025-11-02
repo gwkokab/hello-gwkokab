b3cb40661fe81fa3ff1970ea08d5fa9ba09c9601
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-11-03 03:34:05 +0500

    chore: results of continuous method

--- hbi_continuous_method/frun.sh
+++ hbi_continuous_method/frun.sh
@@ -4,7 +4,7 @@ f_monk_n_pls_m_gs \
 	--seed 37 \
 	--n-pl 1 \
 	--n-g 0 \
-	--data-filename "../generating_mock_posterior_estimates/data/realization_0/posteriors/means_covs.hdf5" \
+	--data-filename "../generating_mock_posterior_estimates/data/realization_0/means_covs.hdf5" \
 	--n-samples 100 \
 	--minimum-mc-error 0.01 \
 	--n-checkpoints 10 \
