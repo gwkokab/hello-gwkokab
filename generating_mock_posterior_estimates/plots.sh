c994ed40d34b2474d9e0a75af4db1d62efb087dd
Muhammad Zeeshan <m.zeeshan5885@gmail.com>
2025-11-03 22:01:02 -0800

    fake PEs generation with eccentricity

--- generating_mock_posterior_estimates/plots.sh
+++ generating_mock_posterior_estimates/plots.sh
@@ -23,3 +23,16 @@ gwk_batch_scatter2d \
 	--use-latex \
 	--alpha 0.1 \
 	--dpi 200
+
+gwk_batch_scatter3d \
+        --data "data/realization_0/posteriors/*.dat" \
+        --output $dir/m1m2ecc_batch_0.png \
+        --x-value-column-name mass_1_source \
+        --y-value-column-name mass_2_source \
+	--z-value-column-name eccentricity \
+        --xlabel "\$m_1 [M_\odot]\$" \
+        --ylabel "\$m_2 [M_\odot]\$" \
+	--zlabel "\$\sigma_\epsilon\$" \
+        --use-latex \
+        --alpha 0.1 \
+        --dpi 200
--- generating_mock_posterior_estimates/run.sh
+++ generating_mock_posterior_estimates/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+export CUDA_VISIBLE_DEVICES=0
+export NPROC=4
+export OPENBLAS_NUM_THREADS=1
 
-genie_n_pls_m_gs \
+# JAX Configuration
+export JAX_COMPILATION_CACHE_DIR="$HOME/jax_cache"
+
+time genie_n_pls_m_gs \
 	--seed 37 \
 	--error-size 10000 \
 	--num-realizations 1 \
 	--model-json model.json \
 	--pmean-json pmean.json \
-	--err-json err.json
+	--err-json err.json \
+	--add-truncated-normal-eccentricity
