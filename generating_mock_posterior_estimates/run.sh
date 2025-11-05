cb740cba053c77b4aaa5a249cfc11aae8a74707c
Muhammad Zeeshan <mzeeshan@Mac.lan>
2025-11-05 12:27:50 -0500

    fixing the seed to reproduce the sample example

--- generating_mock_posterior_estimates/run.sh
+++ generating_mock_posterior_estimates/run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
-export NPROC=4
-export OPENBLAS_NUM_THREADS=1
-
-# JAX Configuration
-export JAX_COMPILATION_CACHE_DIR="$HOME/jax_cache"
 
 time genie_n_pls_m_gs \
-	--seed 37 \
+	--seed 21893 \
 	--error-size 10000 \
 	--num-realizations 1 \
 	--model-json model.json \
