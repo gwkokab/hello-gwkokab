79a15af03b31d6007a8dc4769dd469514210c720
Muhammad Zeeshan <mzeeshan@Mac.lan>
2025-11-06 12:17:54 -0500

    updated configs with eccentricity

--- hbi_continuous_method/frun.sh
+++ hbi_continuous_method/frun.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 f_monk_n_pls_m_gs \
-	--seed 37 \
+	--seed $RANDOM \
 	--n-pl 1 \
 	--n-g 0 \
 	--data-filename "../generating_mock_posterior_estimates/data/realization_0/means_covs.hdf5" \
 	--n-samples 100 \
 	--minimum-mc-error 0.01 \
 	--n-checkpoints 10 \
-	--n-max-steps 3 \
+	--n-max-steps 4 \
 	--pmean-json pmean.json \
 	--prior-json prior.json \
-	--sampler-config flowMC_config.json
+	--sampler-config flowMC_config.json \
+        --add-truncated-normal-eccentricity
